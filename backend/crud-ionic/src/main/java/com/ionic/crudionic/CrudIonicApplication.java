f3e8dc429368139d284977f98a961ee81456aea8
Fernando Silva <fernandoadmti@hotmail.com>
2023-02-26 11:42:20 -0300

    criando backend com spring

---
+++ backend/crud-ionic/src/main/java/com/ionic/crudionic/CrudIonicApplication.java
@@ -0,0 +1,13 @@
+package com.ionic.crudionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudIonicApplication.class, args);
+	}
+
+}
---
+++ backend/crud-ionic/src/test/java/com/ionic/crudionic/CrudIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionic.crudionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
