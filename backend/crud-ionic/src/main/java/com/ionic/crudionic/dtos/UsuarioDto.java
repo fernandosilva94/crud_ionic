45473d1fdd82a7adce405b285186ba891cfde3b2
Fernando Silva <fernandoadmti@hotmail.com>
2023-03-19 22:31:36 -0300

    adicionando get e set do dto

--- backend/crud-ionic/src/main/java/com/ionic/crudionic/dtos/UsuarioDto.java
+++ backend/crud-ionic/src/main/java/com/ionic/crudionic/dtos/UsuarioDto.java
@@ -1,10 +1,12 @@
 package com.ionic.crudionic.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public class UsuarioDto {
 
+  @NotEmpty
   @NotBlank
   @Size(max = 500)
   private String nome;
@@ -16,4 +18,28 @@ public class UsuarioDto {
   @NotBlank
   @Size(min = 4)
   private String senha;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getSenha() {
+    return senha;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
 }
