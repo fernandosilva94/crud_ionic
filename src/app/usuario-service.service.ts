9e84d460c918fea5fe6be7a0826ca82f11975682
Fernando Silva <fernandoadmti@hotmail.com>
2023-03-19 22:33:10 -0300

    ajuste basico

--- src/app/usuario-service.service.ts
+++ src/app/usuario-service.service.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment';
+import { UsuarioModel } from './models/UsuarioModel';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsuarioServiceService {
+export class UsuarioService {
   private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getUsuario() {
     return this.http.get(`${this.apiUrl}/usuario`);
+  };
+
+  salvarUsuario(usuario: UsuarioModel): Observable<UsuarioModel> {
+    return this.http.post<UsuarioModel>(`${this.apiUrl}/usuario`, usuario);
   };
 }
