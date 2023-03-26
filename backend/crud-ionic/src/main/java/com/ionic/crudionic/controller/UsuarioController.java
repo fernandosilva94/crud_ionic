58fb229078326eb3053cf808ddce1ba100ea56cb
Fernando Silva <fernandoadmti@hotmail.com>
2023-03-26 02:31:57 -0300

    adicionado logica no usuariocontroller do back para atualizar usuario

--- backend/crud-ionic/src/main/java/com/ionic/crudionic/controller/UsuarioController.java
+++ backend/crud-ionic/src/main/java/com/ionic/crudionic/controller/UsuarioController.java
@@ -1,11 +1,16 @@
 package com.ionic.crudionic.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +20,6 @@ import com.ionic.crudionic.models.UsuarioModel;
 import com.ionic.crudionic.services.UsuarioService;
 
 import jakarta.validation.Valid;
-import jakarta.validation.Validator;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuario")
@@ -36,5 +38,15 @@ public class UsuarioController {
   @GetMapping
   public ResponseEntity<List<UsuarioModel>> getAllUsuario() {
     return ResponseEntity.status(HttpStatus.OK).body(usuarioService.findAll());
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<UsuarioModel> editarUsuario(@PathVariable Integer id, @RequestBody @Valid UsuarioDto usuarioDto) {
+    var usuarioModel = new UsuarioModel();
+    Optional<UsuarioModel> usuarioModelOptional = usuarioService.findById(id);
+    BeanUtils.copyProperties(usuarioDto, usuarioModel);
+    usuarioModel.setId(usuarioModelOptional.get().getId());
+
+    return ResponseEntity.status(HttpStatus.OK).body(usuarioService.save(usuarioModel));
   }
 }
