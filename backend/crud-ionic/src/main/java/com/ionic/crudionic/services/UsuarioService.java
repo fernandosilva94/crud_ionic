c339b906a22c510439860aef8b619c0a8c8f0a53
Fernando Silva <fernandoadmti@hotmail.com>
2023-03-26 02:35:11 -0300

    criacao do metodo findbyid e do metodo de excecao

--- backend/crud-ionic/src/main/java/com/ionic/crudionic/services/UsuarioService.java
+++ backend/crud-ionic/src/main/java/com/ionic/crudionic/services/UsuarioService.java
@@ -1,13 +1,17 @@
 package com.ionic.crudionic.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ionic.crudionic.Repository.UsuarioRepository;
+import com.ionic.crudionic.dtos.UsuarioDto;
 import com.ionic.crudionic.models.UsuarioModel;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -21,7 +25,27 @@ public class UsuarioService {
     return usuarioRepository.save(usuarioModel);
   }
 
+  public Optional<UsuarioModel> findById(Integer id) {
+    Optional<UsuarioModel> usuarioModelOptional = usuarioRepository.findById(id);
+    try{
+      if(usuarioModelOptional.isPresent()) {
+        return usuarioModelOptional;
+      }
+
+    } catch (Exception e) {
+      throw new MinhaExcecao("Usuário não encontrado.");
+    }
+    return usuarioModelOptional;
+  }
+
   public List<UsuarioModel> findAll() {
     return usuarioRepository.findAll();
   }
+
+  public class MinhaExcecao extends RuntimeException {
+    public MinhaExcecao(String mensagem) {
+        super(mensagem);
+    }
+}
+
 }
