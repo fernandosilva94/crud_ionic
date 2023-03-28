11574d9ec639554c3e99050d9156f283162fb43c
Fernando Silva <fernandoadmti@hotmail.com>
2023-03-27 21:56:33 -0300

    ajustando validacoes dos campos

--- src/app/criar-usuario/criar-usuario.component.ts
+++ src/app/criar-usuario/criar-usuario.component.ts
@@ -13,6 +13,8 @@ import { ValidatorsService } from './../validators.service';
 export class CriarUsuarioComponent implements OnInit {
   formGroup: FormGroup;
   isPasswordVisible: boolean = false;
+  isVisible: boolean = false;
+  isVisibleCampos: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -39,24 +41,39 @@ export class CriarUsuarioComponent implements OnInit {
   }
 
   onSubmit(): void {
+    this.isVisible = false;
     const nome = this.formGroup.get('nome')?.value;
     const email = this.formGroup.get('email')?.value;
     const senha = this.formGroup.get('senha')?.value;
+    const confirmarSenha = this.formGroup.get('confirmarSenha')?.value;
     const usuario: UsuarioModel = { nome, email, senha };
-    console.log("dados do usuario: ",usuario);
 
-    this.usuarioService.salvarUsuario(usuario).subscribe({
-      next: (usuarioSalvo: UsuarioModel) => {
-        console.log('Usuário salvo com sucesso!', usuarioSalvo);
-      },
-      error: (erro) => {
-        console.error('Erro ao salvar usuário:', erro);
-      },
-      complete: () => {
-        console.log('Usuario salvo com sucesso.');
-        location.reload();
+    console.log("dados do usuario: ",usuario);
+    if (nome || nome.trim().length > 0 || email || email.trim().length > 0) {
+      this.isVisibleCampos = false;
+
+      if (senha == confirmarSenha && senha != "" && confirmarSenha != "") {
+        this.isVisible = false;
+        this.usuarioService.salvarUsuario(usuario).subscribe({
+          next: (usuarioSalvo: UsuarioModel) => {
+            console.log('Usuário salvo com sucesso!', usuarioSalvo);
+          },
+          error: (erro) => {
+            console.error('Erro ao salvar usuário:', erro);
+          },
+          complete: () => {
+            console.log('Usuario salvo com sucesso.');
+            location.reload();
+          }
+        });
+      } else {
+        this.isVisible = true;
       }
-    });
+
+    } else {
+      this.isVisibleCampos = true;
+    }
+
   }
 
   btnVoltar() {
