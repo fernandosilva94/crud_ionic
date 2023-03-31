514576866e5961ad7824833e35d485c3269bf2b7
theuslefone <matheuskeeven22@gmail.com>
2023-03-30 21:48:14 -0300

    Edita organização das pastas - pt 1

--- backend/crud-ionic/src/main/java/com/ionic/crudionic/Repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ionic.crudionic.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ionic.crudionic.models.UsuarioModel;
-
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Integer> {}
